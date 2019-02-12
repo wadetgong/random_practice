cc45178340808d596d449cc2c7e947832af24c74
Wade Gong <wadetgong@gmail.com>
2019-02-12 00:58:27 -0500

    initial scaffolding

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`)
+})
